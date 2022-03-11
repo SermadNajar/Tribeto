56e3523af066bfa1310068d23ef1fb3c7b62aedb
SermadNajar <sermadnajar@hotmail.com>
2022-03-11 01:30:55 +0100

    Added a new footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Homepage/Navbar'
 import HeroSection from './components/Homepage/HeroSection'
+import Footer from './components/Homepage/Footer'
 
 export default function App() {
   return (
     <>
     <Navbar />
     <HeroSection />
+    <Footer />
     </>
   )
 }
