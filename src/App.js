4d49b48005c605174400e3140aaf6702ed455b3a
SermadNajar <sermadnajar@hotmail.com>
2022-03-23 23:53:30 +0100

    Dashboard added almost works with bugs

--- src/App.js
+++ src/App.js
@@ -5,23 +5,36 @@ import ErrorPage from "./Pages/ErrorPage";
 import Ctacampaign from "./Pages/Ctacampaign";
 import WithNav from "./components/WithNav";
 import WithoutNav from "./components/WithoutNav";
+import Overview from "./Dashboard/Pages/Overview";
+import Settings from "./Dashboard/Pages/Settings";
 import Dashboard from "./Pages/Dashboard";
+import Footer from "./Dashboard/Inner/Footer";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route element={<WithoutNav />}>
-          <Route path="/Dashboard" element={<Dashboard />} />
-          <Route path="/*" element={<ErrorPage />} />
-        </Route>
-        <Route element={<WithNav />}>
-          <Route index element={<Home />} />
-          <Route path="/Ctacampaign" element={<Ctacampaign />} />
-        </Route>
-        <Route element={<WithNav />}></Route>
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route>
+            // Dashboard
+            <Route path="/Dashboard" element={<Dashboard />}>
+              <Route exact path="/Dashboard/overview" element={<Overview />} />
+              <Route exact path="/Dashboard/settings" element={<Settings />} />
+              <Route path="*/Dashboard" element={<Footer />} />
+            </Route>
+          </Route>
+          // Error page without Main navigaiton bar
+          <Route element={<WithoutNav />}>
+            <Route path="/*" element={<ErrorPage />} />
+          </Route>
+          // Home and rest with Main navigaiton bar
+          <Route element={<WithNav />}>
+            <Route index element={<Home />} />
+            <Route path="/Ctacampaign" element={<Ctacampaign />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
