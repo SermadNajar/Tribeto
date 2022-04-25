a4fffebe8c1a93d7e2bca2e949d426045b671d8b
SermadNajar <sermadnajar@hotmail.com>
2022-04-25 14:42:34 +0200

    Changes to homepage

---
+++ src/components/NavbarWithSearch.jsx
@@ -0,0 +1,163 @@
+import { Fragment } from "react";
+import { Menu, Popover, Transition } from "@headlessui/react";
+import { SearchIcon } from "@heroicons/react/solid";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { useTranslation } from "react-i18next";
+import i18next from "i18next";
+import Toggle from "./toggle";
+import Toggle2 from "./toggle2";
+import React, { useState } from "react";
+import ModalBtn from "./ModalBtn";
+import "flowbite";
+
+const navigation = [
+  { name: "Dashboard", href: "../dashboard", current: false },
+  { name: "Calendar", href: "#", current: false },
+  { name: "Teams", href: "#", current: false },
+  { name: "Directory", href: "#", current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Navbar() {
+  const { t, i18n } = useTranslation();
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  function handleClick(lang) {
+    i18n.changeLanguage(lang);
+  }
+
+  return (
+    <>
+      {/* When the mobile menu is open, add `overflow-hidden` to the `body` element to prevent double scrollbars */}
+      <Popover
+        as="header"
+        className={({ open }) =>
+          classNames(
+            open ? "fixed inset-0 z-40 overflow-y-auto" : "",
+            "bg-white dark:bg-gray-900 shadow-sm dark:shadow-gray-800 sticky top-0 z-50"
+          )
+        }
+      >
+        {({ open }) => (
+          <>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="relative flex justify-between xl:grid xl:grid-cols-12 lg:gap-8">
+                <div className="flex md:absolute md:left-0 md:inset-y-0 lg:static xl:col-span-2">
+                  <div className="flex-shrink-0 flex items-center">
+                    <div>
+                      <Toggle2 />
+                    </div>
+                  </div>
+                </div>
+                <div className="min-w-0 flex-1 md:px-8 lg:px-0 xl:col-span-6">
+                  <div className="flex items-center px-6 py-4 md:max-w-3xl md:mx-auto lg:max-w-none lg:mx-0 xl:px-0">
+                    <div className="w-full">
+                      <label htmlFor="search" className="sr-only">
+                        Search for a company
+                      </label>
+                      <div className="relative">
+                        <div className="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
+                          <SearchIcon
+                            className="h-5 w-5 text-gray-400 dark:text-gray-200"
+                            aria-hidden="true"
+                          />
+                        </div>
+                        <input
+                          id="search"
+                          name="search"
+                          className="block w-full bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-800 rounded-md py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:outline-none focus:text-gray-900 focus:placeholder-gray-400 dark:placeholder-gray-200 focus:ring-1 focus:ring-primary focus:border-primary sm:text-sm"
+                          placeholder="Search for a company"
+                          type="search"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-center md:absolute md:right-0 md:inset-y-0 lg:hidden">
+                  {/* Mobile menu button */}
+                  <Popover.Button className="-mx-2 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary">
+                    <span className="sr-only">Open menu</span>
+                    {open ? (
+                      <XIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Popover.Button>
+                </div>
+                <div className="hidden lg:flex lg:items-center lg:justify-end xl:col-span-4">
+                  {/* Profile dropdown */}
+                  <Menu as="div" className="flex-shrink-0 relative ml-5">
+                    <div>
+                      <div>
+                        <ModalBtn />
+                      </div>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    ></Transition>
+                  </Menu>
+                  {/* Toogle dark mode */}
+                  <Toggle />
+                  {/* Toggle dark mode end */}
+                  <a
+                    href="/ctacampaign"
+                    className="ml-6 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-gray-900 dark:bg-primary hover:bg-primary dark:hover:bg-secondary dark:focus:ring-offset-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
+                  >
+                    FOR COMPANIES
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <Popover.Panel as="nav" className="lg:hidden" aria-label="Global">
+              <div className="max-w-3xl mx-auto px-2 pt-2 pb-3 space-y-1 sm:px-4">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    aria-current={item.current ? "page" : undefined}
+                    className={classNames(
+                      item.current
+                        ? "bg-gray-100 text-gray-900"
+                        : "hover:bg-gray-50",
+                      "block rounded-md py-2 px-3 text-base font-medium"
+                    )}
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div className="border-t border-gray-200 pt-4 pb-3">
+                <div className="max-w-3xl mx-auto px-4 flex items-center sm:px-6">
+                  <div className="max-w-3xl">
+                    <div>
+                      <ModalBtn />
+                    </div>
+                  </div>
+                  {/* Toogle dark mode */}
+                  <div className="ml-auto">
+                    <Toggle />
+                  </div>
+                  {/* Toggle dark mode end */}
+                </div>
+              </div>
+            </Popover.Panel>
+          </>
+        )}
+      </Popover>
+    </>
+  );
+}
--- src/components/Review/RecentReviews.jsx
+++ src/components/Review/RecentReviews.jsx
@@ -1,20 +1,88 @@
-import ReviewCard from "../Review/ReviewCard";
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-export default function Recent() {
+import ReviewCard from "../Review/ReviewCard";
+import ReviewCard2 from "../Review/ReviewCard2";
+import ReviewCard3 from "../Review/ReviewCard3";
+import ReviewCard4 from "../Review/ReviewCard4";
+
+// Import Swiper styles
+import "swiper/css";
+
+// import required modules
+import { Autoplay, FreeMode } from "swiper";
+
+export default function App() {
   return (
     <>
-      <div className="my-8 p-4">
-        <div className="grid gap-16 sm:grid-cols-2 lg:grid-cols-8 md:grid-cols-8">
+    
+      <Swiper
+        spaceBetween={30}
+        slidesPerGroup={1}
+        freemode={{
+          enabled: true,
+          speed: 5000,
+        }}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        loopFillGroupWithBlank={true}
+        breakpoints={{
+          // when window width is >= 320px
+          320: {
+            slidesPerView: 2,
+            spaceBetween: 150,
+          },
+          // when window width is >= 480px
+          480: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          // when window width is >= 640px
+          640: {
+            slidesPerView: 4,
+            spaceBetween: 180,
+          },
+          768: {
+            slidesPerView: 5,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 7,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Autoplay, FreeMode]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
           <ReviewCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ReviewCard2 />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ReviewCard3 />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ReviewCard4 />
+        </SwiperSlide>
+        <SwiperSlide>
           <ReviewCard />
-          <ReviewCard />
-          <ReviewCard />
-          <ReviewCard />
-          <ReviewCard />
-          <ReviewCard />
-          <ReviewCard />
-        </div>
-      </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <ReviewCard2 />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ReviewCard3 />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ReviewCard4 />
+        </SwiperSlide>
+      </Swiper>
     </>
   );
 }
